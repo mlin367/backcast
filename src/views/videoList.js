635d74dff0c09bf90da15fb6f5e6cb006895443c
Hack Reactor Student <student@hackreactor.com>
2018-10-01 15:22:40 -0700

    rendering videoListEntries

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -4,7 +4,7 @@ var VideoListView = Backbone.View.extend({
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
-    this.$el.find('.video-list').html(this.collection.map(function(videoListEntry){
+    this.$el.find('.video-list').html(this.collection.map(function(videoListEntry) {
       var videoListEntryView = new VideoListEntryView({ model: videoListEntry});
       return videoListEntryView.render().$el;
     }));
