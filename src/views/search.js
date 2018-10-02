0a94b5d57f5498f37e64e6b40f21ed07a964a0b1
Hack Reactor Student <student@hackreactor.com>
2018-10-02 12:31:13 -0700

    passing all tests

--- src/views/search.js
+++ src/views/search.js
@@ -3,10 +3,17 @@ var SearchView = Backbone.View.extend({
 
   render: function() {
     this.$el.html(this.template());
-    this.$el.find('.btn').on('click', function(e) {
-      // console.log(this.$el.find('.form-control').val())
+    this.$el.find('button').on('click', function() {
       this.collection.search(this.$el.find('.form-control').val());
     }.bind(this));
+
+    this.$el.find('input').on('keyup', function(e) {
+      var code = e.keyCode || e.which;
+      if (code === 13) { 
+        this.collection.search(this.$el.find('.form-control').val());
+      }
+    }.bind(this));
+
     return this;
   },
 
