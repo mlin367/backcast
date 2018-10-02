c16631aaa71cf206782fa77c5451d054f52ea46e
Hack Reactor Student <student@hackreactor.com>
2018-10-01 19:19:46 -0700

    displaying video player after click

--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -2,7 +2,7 @@ var VideoListEntryView = Backbone.View.extend({
 
   render: function() {
     this.$el.html(this.template(this.model.attributes));
-    this.$el.find('.video-list-entry-title').on('click', function(){
+    this.$el.find('.video-list-entry-title').on('click', function() {
       this.model.select();
     }.bind(this)); //TODO this.model.select.bind(this));
     return this;
