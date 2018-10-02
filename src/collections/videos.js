65b06d8107420a5c60826fb60060442827614969
Hack Reactor Student <student@hackreactor.com>
2018-10-02 12:05:43 -0700

    separated search and fetch

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -2,29 +2,31 @@ var Videos = Backbone.Collection.extend({
 
   model: Video,
 
-  // initialize: function() {
-  //   this.on('sync', function() {
-  //     this.search();
-  //   }, this);
-  // },
+  initialize: function() {
+    this.on('sync', function() {
+      this.at(0).select();
+    }, this);
+  },
 
-  search: function(searchString) {
+  fetch: function(dataObject) {
+    var data = {
+      part: 'snippet',
+      maxResults: 5,
+      key: window.YOUTUBE_API_KEY
+    };
+
+    _.extend(data, dataObject.data);
+    
     Backbone.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: 'https://www.googleapis.com/youtube/v3/search',
       type: 'GET',
-      data: {
-        part: 'snippet',
-        query: 'hack reactor',
-        maxResults: 5,
-        key: window.YOUTUBE_API_KEY
-      },
+      data: data,
       contentType: 'application/json',
       success: function (data) {
         this.reset(data.items);
         this.trigger('sync');
 
-        this.at(0).select();
 
         console.log('search successful');
       }.bind(this),
@@ -33,5 +35,9 @@ var Videos = Backbone.Collection.extend({
         console.error('search failed', data.responseText);
       }
     });
+  },
+
+  search: function(searchString) {
+    this.fetch({data: { q: searchString}});
   },
 });
